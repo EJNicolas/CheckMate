7686a518c339df25d8cc361f121f6007db158a5b
Thanh <Thanhgtruong@gmail.com>
2021-12-11 12:06:12 -0800

    Queries
    Sessions are still an issue

--- match-details.php
+++ match-details.php
@@ -1,6 +1,6 @@
 <?php
 include("header.php");
-  $db = mysqli_connect("localhost", "root", "", "new_chess_data");
+  $db = mysqli_connect("localhost", "root", "", "chess-games");
   if($db->connect_errno) {
       $msg = "Database connection failed: ";
       $msg .= mysqli_connect_error();
@@ -8,6 +8,12 @@ include("header.php");
       exit($msg);
   }
 if( isset($_GET['id'])) $id=$_GET['id'];
+if( isset($_POST['commentMessage'])) $id=$_POST['commentMessage'];
+if( isset($_SESSION['email'])){
+          $name = $_SESSION['username'];
+          $email = $_SESSION['email'];
+          echo "Signed in as: " . $name . "</br>";
+}
 
 $limit = 10;
 // $page = ($page - 1) * $limit;
@@ -64,6 +70,62 @@ $limit = 10;
         }
       ?>
       <h2>Comments</h2>
+      <?php
+      if (isset($_SESSION)){
+        echo "<form action=\"match-details.php\" method=\"post\">
+          <table>
+            <tr><td>Add Comment
+              <input type=\"textarea\" rows=\"4\" cols=\"30\" name=\"commentMessage\">
+            </td></tr>
+          </table>
+          <input type=\"submit\">
+        </form>";
+      }
+        ?>
+      <?php
+      echo $email;
+
+      $insertStringA = "INSERT INTO comment_message(username, dateTime, message) VALUES ('username', 'dateTime', 'message')";
+      $insertStringB = "INSERT INTO comments(email, cid, gameId) VALUES ( 'email', 'cid', 'gameId')";
+
+      $commentQuery='';
+       $commentQuery = "SELECT *";
+
+      //from portion
+        $commentQuery .= " FROM comments, comment_message ";
+      //where portion
+        $commentQuery .= "WHERE comments.gameId = '" . $id;
+
+        $commentQuery .= "' ORDER BY comment_message.dateTime ;" ;
+
+          //print the comments for this game
+
+        if(!empty($commentQuery)){
+          $commentStatement = mysqli_prepare($db, $commentQuery);
+          if(!$commentStatement) {
+            die("Error is:". mysqli_error($db) );
+          }
+          mysqli_stmt_execute($commentStatement);
+          $commentResults = mysqli_stmt_get_result($commentStatement);
+          if(mysqli_num_rows($commentResults) != 0) {
+            
+            //make only the useful searchConditions
+            while($commentRow = mysqli_fetch_assoc($commentResults)) {
+              //only check if the key exists.
+              echo "<table><tr>";
+              echo "<tr><td>" .$commentRow['username']."</td>";
+              echo "<tr><td>" . $commentRow['commentMessage'] ."</td></tr>"; 
+              echo "</tr>";
+              echo "  </tr></table>";
+              }
+            }
+            // echo " </td></tr></table>";
+            
+          mysqli_free_result($commentResults);
+          mysqli_stmt_close($commentStatement);
+
+        }
+      ?>
   <?php
   include("footer.php");
   ?>
