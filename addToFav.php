b7e5c8b0038646f08b81b88eb0b0c844cdf22a6a
Thanh <Thanhgtruong@gmail.com>
2021-12-13 12:00:41 -0800

    Used Javascript for Fav
    fucken missing bracket

---
+++ addToFav.php
@@ -0,0 +1,44 @@
+<?php
+  $db = mysqli_connect("localhost", "root", "", "chess-games");
+  if($db->connect_errno) {
+      $msg = "Database connection failed: ";
+      $msg .= mysqli_connect_error();
+      $msg .= " (" . mysqli_connect_errno() . ")";
+      exit($msg);
+  }
+  session_start();
+  if( isset($_GET['id'])) $id=$_GET['id'];
+  if( isset($_SESSION['email'])){
+          $name = $_SESSION['username'];
+          $email = $_SESSION['email'];
+    }
+    // echo "<script>console.log('test')</script>";
+
+    $check = "SELECT * FROM favourites WHERE email='$email' AND gameId='$id'";
+    if(!empty($check)){
+        $insertCheck = mysqli_prepare($db, $check);
+        if(!$insertCheck) {
+          die("Error is:". mysqli_error($db) );
+        }
+        mysqli_stmt_execute($insertCheck);
+        $checkResults = mysqli_stmt_get_result($insertCheck);
+          if(mysqli_num_rows($checkResults) == 0) {          
+            //make only the useful searchConditions
+            $added = "INSERT INTO favourites(email, gameId) VALUES ('$email', '$id')";
+            // echo $added;
+            if(!empty($added)){
+            $insertAdded = mysqli_prepare($db, $added);
+            if(!$insertAdded) {
+                die("Error is:". mysqli_error($db) );
+            }
+            mysqli_stmt_execute($insertAdded);
+            mysqli_stmt_close($insertAdded);
+        }
+            }
+        mysqli_free_result($checkResults);
+        mysqli_stmt_close($insertCheck);
+    }
+
+    
+
+?>
---
+++ headerDetails.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title> CheckMate </title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="js/favourites.js"></script>
+  </head>
+  <body>
+    <nav>
+      <?php
+        if(isset($_SESSION['email'])){
+          $name = $_SESSION['username'];
+          $email = $_SESSION['email'];
+          echo "Signed in as: " . $name . "</br>";
+        }
+      ?>
+      <a href="home.php">Home</a>
+      <a href="find-game.php">Search Games</a>
+      <a href="profile.php">My Profile</a>
+      <a href="login.php">Log In</a>
+      <a href="logout.php">Log Out</a>
+      <a href="change-status.php">Change Online Status</a>
+      <?php
+
+      ?>
+    </nav>
