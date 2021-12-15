6bf7daec07a3aa9ec7d2f99797ba453c55c01ab3
EJ Nicolas <ejnicolas001@gmail.com>
2021-12-14 21:06:53 -0800

    Fixed small bugs in adding to favourites

--- back-end/removeFromFave.php
+++ back-end/removeFromFave.php
@@ -13,7 +13,7 @@
           $email = htmlspecialchars($_SESSION['email']);
     }
     // echo "<script>console.log('test')</script>";
-    $check = "SELECT * FROM favourites WHERE email='$email' AND gameId='$id'";
+    $check = "SELECT * FROM favourites WHERE email='$email' AND gameID='$id'";
     if(!empty($check)){
         $insertCheck = mysqli_prepare($db, $check);
         if(!$insertCheck) {
@@ -21,9 +21,9 @@
         }
         mysqli_stmt_execute($insertCheck);
         $checkResults = mysqli_stmt_get_result($insertCheck);
-          if(mysqli_num_rows($checkResults) != 0) {          
+          if(mysqli_num_rows($checkResults) != 0) {
             //make only the useful searchConditions
-            $delete = "DELETE FROM favourites WHERE email='$email' AND gameId='$id'";
+            $delete = "DELETE FROM favourites WHERE email='$email' AND gameID='$id'";
             // echo $added;
             if(!empty($delete)){
             $insertDelete = mysqli_prepare($db, $delete);
@@ -38,6 +38,6 @@
         mysqli_stmt_close($insertCheck);
     }
 
-    
+
 
 ?>
